85b754119456ffb425653af8ee0fcbc7075d7bf5
RanaHisham18 <hishamrana00@gmail.com>
2023-10-26 16:20:57 +0300

    fix

--- app/src/main/java/com/example/whereami/MainActivity.kt
+++ app/src/main/java/com/example/whereami/MainActivity.kt
@@ -1,85 +1,82 @@
 package com.example.whereami
 
-import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.telephony.TelephonyManager
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.whereami.databinding.ActivityMainBinding
 import java.util.Locale
+import android.Manifest
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private val SIM_PERMISSION_CODE = 101
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-            val view = binding.root
-                setContentView(view)
+        val view = binding.root
+        setContentView(view)
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_PHONE_STATE), SIM_PERMISSION_CODE)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_PHONE_STATE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_PHONE_STATE),
+                SIM_PERMISSION_CODE
+            )
         } else {
-            // Permission is already granted, you can access SIM card data here
-            // Access SIM card data code goes here
-            getUserCountry(applicationContext)
             val userCountryCode = getUserCountry(this)
-            binding.button.setOnClickListener{
-                binding.textView.text = userCountryCode
-
+            binding.button.setOnClickListener {
+                if (userCountryCode != null) {
+                    binding.textView.text = userCountryCode
+                } else {
+                    binding.textView.text = "Can't get the location"
+                }
             }
-
         }
     }
 
-    // Handle permission request result
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == SIM_PERMISSION_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted, access SIM card data here
-                // Access SIM card data code goes here
+
             } else {
-                // Permission denied, show a message to the user
+
                 Toast.makeText(this, "Permission Denied", Toast.LENGTH_LONG).show()
             }
         }
     }
 
-     fun getUserCountry(context: Context): String? {
-
-         val msg = Toast.makeText(this, "Can't get the location", Toast.LENGTH_LONG).show()
+    fun getUserCountry(context: Context): String? {
         try {
             val tm = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
             val simCountry = tm.simCountryIso
             if (simCountry != null && simCountry.length == 2) { // SIM country code is available
-                return simCountry.lowercase(Locale.US)
+                val countryCode = simCountry.lowercase(Locale.US)
+                binding.textView.text = countryCode
+                return countryCode
             } else if (tm.phoneType != TelephonyManager.PHONE_TYPE_CDMA) { // device is not 3G (would be unreliable)
                 val networkCountry = tm.networkCountryIso
                 if (networkCountry != null && networkCountry.length == 2) { // network country code is available
-                    return networkCountry.lowercase(Locale.US)
+                    val countryCode = networkCountry.lowercase(Locale.US)
+                    binding.textView.text = countryCode
+                    return countryCode
                 }
             }
         } catch (e: Exception) {
-           // Toast.makeText(this, "Can't get the location", Toast.LENGTH_LONG).show()
-
         }
-        return msg.toString()
+        return null
     }
-
 }
-
-
-
-
-
-
-
-
-
-
